61930da37b1e88342e5c1d7cce72226f7dd80065
timwolla <timwolla@mail.develfusion.com>
2011-02-08 15:10:31 +0100

    Added doc in chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -1,14 +1,29 @@
 <?php
+/**
+ * Initializes the bot
+ *
+ * @author		Tim Düsterhus
+ * @copyright	2010 Tim Düsterhus
+ */
+ 
 date_default_timezone_set('Europe/Berlin');
+
+// get the signal handler working
 declare(ticks = 1);
 define('DIR', dirname(__FILE__).'/');
+
+// load userconfig
 require_once(DIR.'config/userdata.php');
 define('TIME', time());
+
+// write pidfile
 file_put_contents(DIR.'config/bot.pid', getmypid());
+
 function __autoload($class) {
 	require_once(DIR.'lib/'.$class.'.class.php');
 }
 
 set_error_handler(array('Core', 'handleError'));
-#set_exception_handler(array('Core', 'handleException'));
+
+// start up 
 Core::get();
