d997cc5952fda34d080ea56a97a82dc5e77abcb2
TimWolla <timwolla@arcor.de>
2011-02-08 17:17:45 +0100

    You can tell the Bot now to join a room

--- lib/ModuleOp.class.php
+++ lib/ModuleOp.class.php
@@ -8,13 +8,18 @@ class ModuleOp extends Module {
 			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
 			exit;
 		}
-		else if (substr(Module::removeWhisper($bot->message['text']), 0, 4) == '!say') {
+		else if (substr(Module::removeWhisper($bot->message['text']), 0, 5) == '!say ') {
 			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
 			$bot->queue(substr(Module::removeWhisper($bot->message['text']), 5));
 		}
 		else if (Module::removeWhisper($bot->message['text']) == '!loaded') {
 			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
 			$bot->queue('/whisper "'.$bot->message['usernameraw'].'" Folgende Module sind geladen: '.implode(', ', array_keys(Core::getModules())));
+		}
+		else if (substr(Module::removeWhisper($bot->message['text']), 0, 6) == '!join ') {
+			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			$bot->getConnection()->join(substr(Module::removeWhisper($bot->message['text']), 6));
+			$bot->success();
 		}
 	}
 }
