8d41065ceeca68c9c9c805704dce93544e665611
TimWolla <timwolla@arcor.de>
2011-02-08 18:33:45 +0100

    Added Util

---
+++ lib/ModuleUtil.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class ModuleUtil extends Module {
+	public function destruct() {
+	
+	}
+	
+	public function handle(Bot $bot) {
+		if ($bot->message['text'] == '!mycolor') {
+			preg_match_all('/color: #[0-9a-fA-F]{6}/', $bot->message['username'], $matches);
+			$bot->queue('/whisper "'.$bot->message['usernameraw'].'" /color '.substr($matches[0][0], 7).' '.substr($matches[0][count($matches[0])-1], 7));
+		}
+	}
+}
