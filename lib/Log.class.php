bcf6fa34df4ddd53d7fdb429bc6e5bdac3786574
TimWolla <timwolla@arcor.de>
2011-02-24 15:55:37 +0100

    PHPDoc for log

--- lib/Log.class.php
+++ lib/Log.class.php
@@ -1,8 +1,21 @@
 <?php
+/**
+ * Core-class. Provides basic module handling
+ *
+ * @author	Tim Düsterhus
+ * @copyright	2010 - 2011 Tim Düsterhus
+ */
 class Log {
 	
+	/**
+	 * Logs an action
+	 *
+	 * @param	string	$log		log-name
+	 * @param	string	$what	message
+	 * @return	void
+	 */
 	public function log($log, $what) {
-	echo	"[".date('d.m.Y H:i:s')."] ".$log.": ".$what."\n";
+		echo	"[".date('d.m.Y H:i:s')."] ".$log.": ".$what."\n";
 		$data = '';
 		if (file_exists(DIR.'log/'.$log)) {
 			$data = file_get_contents(DIR.'log/'.$log);
@@ -10,14 +23,26 @@ class Log {
 		file_put_contents(DIR.'log/'.$log, $data."\n[".date('d.m.Y H:i:s')."] ".$what);
 	}
 	
+	/**
+	 * @see	Log::log()
+	 */
 	public function __set($log, $what) {
 		$this->log($log, $what);
 	}
 	
+	/**
+	 * @see	Log::clear()
+	 */
 	public function __unset($log) {
 		$this->clear($log);
 	}
 	
+	/**
+	 * Clears the log
+	 *
+	 * @param	string	$log		log-name
+	 * @return	void
+	 */
 	public function clear($log) {
 		unlink(DIR.'log/'.$log);
 	}
