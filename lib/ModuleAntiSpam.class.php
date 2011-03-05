d5b5d6d08ff37b039ddab6ffedd3e23a62b03845
TimWolla <timwolla@arcor.de>
2011-03-05 13:46:11 +0100

    Added AntiSpam

---
+++ lib/ModuleAntiSpam.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class ModuleAntiSpam extends Module implements AlwaysFire {
+	protected $config = null;
+	protected $caps = array();
+
+	public function __construct() {
+		$this->config = new Config('antispam', array('capspercent' => 80));
+		$this->config->write();
+	}
+
+	public function destruct() {
+		$this->config->write();
+	}
+
+	public function handle(Bot $bot) {
+		if ($bot->message['id'] % 500 == 0) $this->config->write();
+		if ($bot->message['usernameraw'] == NAME) return;
+		// TODO: Command for config-change
+		$this->caps($bot);
+	}
+	
+	protected function caps(Bot $bot) {
+		if (!isset($this->caps[$bot->message['usernameraw']])) {
+			$this->caps[$bot->message['usernameraw']] = array('kills' => 0, 'counter' => 0, 'lastreset' => 0);
+		}
+		if ($this->caps[$bot->message['usernameraw']]['lastreset'] < (time() - 3600)) {
+			if ($this->caps[$bot->message['usernameraw']]['kills'] > 0) $this->caps[$bot->message['usernameraw']]['kills']--;
+		}
+		if ($this->caps[$bot->message['usernameraw']]['lastreset'] < (time() - 120)) {
+			if ($this->caps[$bot->message['usernameraw']]['counter'] > 0) $this->caps[$bot->message['usernameraw']]['counter']--;
+			$this->caps[$bot->message['usernameraw']]['lastreset'] = time();
+		}
+		
+		$capscount = 0;
+		// don't count whitespace
+		$message = preg_replace('~[\s!?\.]~', '', $bot->message['text']);
+		$charcount = strlen($message);
+		$chars = str_split($bot->message['text']);
+		foreach ($chars as $char) {
+			if (preg_match('~[A-Z]~', $char)) $capscount++;
+		}
+
+		if (strlen($message) < 6 || ($capscount / $charcount * 100) < $this->config->config['capspercent']) return;
+		
+		$this->caps[$bot->message['usernameraw']]['counter']++;
+		$bot->queue('/whisper "'.$bot->message['usernameraw'].'" Bitte unterlasse es zu capsen');
+		if ($this->caps[$bot->message['usernameraw']]['counter'] > 2) {
+			$this->caps[$bot->message['usernameraw']]['counter'] = 0;
+			$bot->queue('/tmute '.$bot->message['usernameraw'].' '.pow(2, $this->caps[$bot->message['usernameraw']]['kills']++));
+		}
+	}
+}
+?>
