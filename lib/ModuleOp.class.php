c4705804ef5487b553a9a697fc1ec9819da10060
TimWolla <timwolla@arcor.de>
2011-04-05 22:07:12 +0200

    Protected some commands

--- lib/ModuleOp.class.php
+++ lib/ModuleOp.class.php
@@ -5,30 +5,30 @@ class ModuleOp extends Module {
 	}
 	public function handle(Bot $bot) {
 		if (Module::removeWhisper($bot->message['text']) == '!shutdown') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 500)) return $bot->denied();
 			Core::log()->info = $bot->message['usernameraw'].' shutted the bot down';
 			$bot->shutdown();
 		}
 		else if (Module::removeWhisper($bot->message['text']) == '!restart') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 500)) return $bot->denied();
                         Core::log()->info = $bot->message['usernameraw'].' restarted the bot';
                         $bot->shutdown(SIGUSR1);
 		}
 		else if (substr(Module::removeWhisper($bot->message['text']), 0, 5) == '!say ') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 500)) return $bot->denied();
 			$bot->queue(substr(Module::removeWhisper($bot->message['text']), 5));
 		}
 		else if (Module::removeWhisper($bot->message['text']) == '!loaded') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 500)) return $bot->denied();
 			$bot->queue('/whisper "'.$bot->message['usernameraw'].'" Folgende Module sind geladen: '.implode(', ', array_keys(Core::getModules())));
 		}
 		else if (substr(Module::removeWhisper($bot->message['text']), 0, 6) == '!join ') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 450)) return $bot->denied();
 			$bot->getConnection()->join(substr(Module::removeWhisper($bot->message['text']), 6));
 			$bot->success();
 		}
 		else if (Module::removeWhisper($bot->message['text']) == '!rooms') {
-			if (!Core::isOp($bot->lookUpUserID())) return $bot->denied();
+			if (!Core::compareLevel($bot->lookUpUserID(), 450)) return $bot->denied();
 			$rooms = $bot->getConnection()->getRooms();
 			$roomString = array();
 			foreach ($rooms as $id => $name) {
