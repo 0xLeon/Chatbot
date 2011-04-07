3228cdac3e6831cec54ab4672fedfac4790414a0
TimWolla <timwolla@arcor.de>
2011-04-07 22:40:28 +0200

    Language for util

--- lib/module/ModuleUtil.class.php
+++ lib/module/ModuleUtil.class.php
@@ -11,10 +11,10 @@ class ModuleUtil extends Module {
 			$bot->queue('/whisper "'.$bot->message['usernameraw'].'" /color '.substr($matches[0][0], 7).' '.substr($matches[0][count($matches[0])-1], 7));
 		}
 		else if ($bot->message['text'] == '!info') {
-			$bot->queue("Information:");
-			$bot->queue("Online seit: ".date('d.m.Y H:i:s', TIME));
-			$bot->queue("Gelesene Nachrichten: ".$bot->messageCount.' ('.round($bot->messageCount / (time() - TIME) * 60, 4).'/m)');
-			$bot->queue("Gesendete Nachrichten: ".$bot->sendCount.' ('.round($bot->sendCount / (time() - TIME) * 60, 4).'/m)');
+			$bot->queue(Core::language()->util_information.':');
+			$bot->queue(Core::language()->util_since.": ".date('d.m.Y H:i:s', TIME));
+			$bot->queue(Core::language()->util_sent.": ".$bot->messageCount.' ('.round($bot->messageCount / (time() - TIME) * 60, 4).'/m)');
+			$bot->queue(Core::language()->util_got.": ".$bot->sendCount.' ('.round($bot->sendCount / (time() - TIME) * 60, 4).'/m)');
 		}
 	}
 }
