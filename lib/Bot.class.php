5f42ac6b70d92647f8f91fcdc5ad09f2af23a556
TimWolla <timwolla@arcor.de>
2011-04-09 12:20:39 +0200

    Even more values

--- lib/Bot.class.php
+++ lib/Bot.class.php
@@ -428,7 +428,7 @@ class Bot {
 		stream_select($read, $write, $except, $tv);
 		if (count($read)) {
 			$data = @fread(STDIN, 1500);
-			$this->data['messages'][] = array('usernameraw' => NAME, 'username' => NAME, 'text' => $data, 'type' => 0, 'roomID' => 0);
+			$this->data['messages'][] = array('id' => 500, 'usernameraw' => NAME, 'username' => NAME, 'text' => $data, 'type' => 0, 'roomID' => 0);
 		}
 	}
 }
