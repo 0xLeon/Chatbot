90516dfa953856918b5bf9ce589f4370ef2364a2
TimWolla <timwolla@arcor.de>
2011-04-12 17:18:13 +0200

    Added missing brackets

--- lib/module/ModuleGoogle.class.php
+++ lib/module/ModuleGoogle.class.php
@@ -10,7 +10,7 @@ class ModuleGoogle extends Module {
 		if ($bot->message['usernameraw'] == NAME) return;
 		if (substr($bot->message['text'], 0, 8) == '!google ') {
 			$data = $this->search(substr($bot->message['text'], 8));
-			$bot->queue('Google ['.substr($bot->message['text'], 8).']: http://google.de/search?q='.rawurlencode(substr($bot->message, 8)).' Ungefähr '.number_format($data['responseData']['cursor']['estimatedResultCount'], 0, Core::language()->decimal_point, Core::language->thousand_separator).' Ergebnisse');
+			$bot->queue('Google ['.substr($bot->message['text'], 8).']: http://google.de/search?q='.rawurlencode(substr($bot->message, 8)).' Ungefähr '.number_format($data['responseData']['cursor']['estimatedResultCount'], 0, Core::language()->decimal_point, Core::language()->thousand_separator).' Ergebnisse');
 			$bot->queue('#1: '.strip_tags($data['responseData']['results'][0]['title']).': '.$data['responseData']['results'][0]['unescapedUrl']);
 		}
 	}
