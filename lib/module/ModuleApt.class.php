2a327aec1383355a6ffdae9fede89bf7246854a0
TimWolla <timwolla@arcor.de>
2011-05-04 18:03:03 +0200

    Install, remove and purge

--- lib/module/ModuleApt.class.php
+++ lib/module/ModuleApt.class.php
@@ -12,7 +12,38 @@ class ModuleApt extends Module {
 
 	public function handle(Bot $bot) {
 		if (substr($bot->message['text'], 0, 7) != 'apt-get') return;
-		$text = substr($bot->message['text'], 7);
+		$text = explode(' ', substr($bot->message['text'], 7), 2);
+		
+		switch ($text[0]) {
+			case 'moo':
+				$bot->queue('...."Have you mooed today?"...');
+			break;
+			case 'install':
+				if (!Core::compareLevel($bot->lookUpUserID(), 'op.load')) return $bot->denied();
+				Core::log()->info = $this->message['usernameraw'].' loaded a module';
+				$result = Core::loadModule(trim($text[1]));
+				if (!is_int($result)) {
+					$this->success();
+				}
+				else {
+					$name = 'module_error_'.$result;
+					$this->queue('/whisper "'.$this->message['usernameraw'].'" '.Core::language()->$name);
+				}
+			break;
+			case 'remove':
+			case 'purge':
+				if (!Core::compareLevel($bot->lookUpUserID(), 'op.load')) return $bot->denied();
+				Core::log()->info = $this->message['usernameraw'].' unloaded a module';
+				$result = Core::unloadModule(trim($text[1]));
+				if (!is_int($result)) {
+					$this->success();
+				}
+				else {
+					$name = 'module_error_'.$result;
+					$this->queue('/whisper "'.$this->message['usernameraw'].'" '.Core::language()->$name);
+				}
+			break;
+		}
 	}
 }
 ?>
