ad306dd9309bf147481e4a2e3bfbbb81e1edc7c9
TimWolla <timwolla@arcor.de>
2011-06-15 22:30:39 +0200

    Added basics for rose module

---
+++ lib/module/ModuleRose.class.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Posts a message when a user joins
+ *
+ * @author	Tim Düsterhus
+ * @copyright	2010 - 2011 Tim Düsterhus
+ */
+class ModuleRose extends Module {
+	public $config = null;
+
+	public function __construct() {
+		$this->config = new Config('rose', array());
+		$this->config->write();
+	}
+	
+	public function destruct() {
+		$this->config->write();
+	}
+	
+	public function handle(Bot $bot) {
+		if ($bot->message['id'] % 500 == 0) $this->config->write();
+		if ($bot->message['type'] == Bot::JOIN) {
+			$userID = $bot->lookUpUserID();
+			if (!isset($this->config->config[$userID])) {
+				$this->config->config[$userID] = array('got' => 0, 'has' => 3);
+			}
+		}
+		if (substr($bot->message['text'], 0, 6) == '!roses') {
+			if ($bot->message['text'] == '!roses') {
+				$userID = $bot->lookUpUserID();
+				$username = $bot->message['usernameraw'];
+			}
+			else {
+				$username = substr($bot->message['text'], 7);
+				$userID = $bot->lookUpUserID($username);
+			}
+			if (!isset($this->config->config[$userID])) {
+				$this->config->config[$userID] = array('got' => 0, 'has' => 3);
+			}
+			$bot->queue('['.$username.'] hat bisher '.$this->config->config[$userID]['got']. ' Rosen erhalten und kann noch '.$this->config->config[$userID]['has'].' Stück verteilen.');
+		}
+		else if (substr($bot->message['text'], 0, 6) == '!rose ') {
+			$userID = $bot->lookUpUserID();
+			if (!isset($this->config->config[$userID])) {
+				$this->config->config[$userID] = array('got' => 0, 'has' => 3);
+			}
+			if ($this->config->config[$userID]['has'] > 0) {
+				$username = substr($bot->message['text'], 6);
+				$to = $bot->lookUpUserID($username);
+				
+				if ($to) {
+					if ($to != $userID) {
+						if (!isset($this->config->config[$to])) {
+							$this->config->config[$to] = array('got' => 0, 'has' => 3);
+						}
+						$this->config->config[$to]['got']++;
+						$bot->queue('['.$bot->message['usernameraw'].'] hat eine Rose an '.$username.' gegeben');
+					}
+					else {
+						$bot->queue('/whisper "'.$bot->message['usernameraw'].'" Du kannst dir nicht selber eine Rose geben');
+					}
+				}
+				else {
+					$bot->queue('/whisper "'.$bot->message['usernameraw'].'" '.Core::language()->get('user_not_found', array('{user}' => $username)));
+				}
+			}
+			else {
+				$bot->queue('/whisper "'.$bot->message['usernameraw'].'" Du hast keine Rosen');
+			}
+		}
+	}
+}
+?>
